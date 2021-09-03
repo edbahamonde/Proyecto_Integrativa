0f9168499eeea0505fb646875c5ca0e7b85615ab
edbahamonde <49206926+edbahamonde@users.noreply.github.com>
2021-09-03 14:36:03 -0500

    Reloj SECCION
    Se creó el reloj contador

--- angular/src/app/reloj/reloj.component.ts
+++ angular/src/app/reloj/reloj.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-reloj',
   templateUrl: './reloj.component.html',
   styleUrls: ['./reloj.component.scss']
 })
-export class RelojComponent implements OnInit {
+export class RelojComponent{
+  title = 'Watch';
 
-  constructor() { }
+  interval;
+  time = new Date(null);
 
-  ngOnInit(): void {
+  startTimer(){
+    this.interval = setInterval(() => {
+      this.time.setSeconds(this.time.getSeconds() + 1);
+    }, 1000);
   }
 
+  pauseTimer(){
+    clearInterval(this.interval);
+  }
+
+  resetTimer(){
+    this.time.setSeconds(0);
+  }
 }
